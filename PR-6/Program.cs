fc0ace40a4f41d350c20358e355fb3b0fb2205ae
Bhautik Ranpara <ranparabhautik30@gmail.com>
2026-03-26 19:45:30 +0530

    Enhance comments in Main method
    Added comment to main method and clarified event handler assignment.

--- PR-6/Program.cs
+++ PR-6/Program.cs
@@ -1,12 +1,12 @@
 ﻿internal class Program
 {
 
-    static void Main(string[] args)
+    static void Main(string[] args) // main method which runs program
     {
         Console.WriteLine("\t\t----- Practical-6 ----- Events -----");
         ProcessBusinessLogic pbl = new ProcessBusinessLogic();
-        Subscriber sub = new Subscriber();
-        pbl.ProcessCompleted += sub.bl_ProcessComplete;
+        Subscriber sub = new Subscriber(); 
+        pbl.ProcessCompleted += sub.bl_ProcessComplete;// assigning the event handler
         pbl.startProcess();
         Console.WriteLine("\tHello from Main");
     }
